64ea0e2128afa3fbf3eb65966822321ddc753739
Eduardo <eduardodiazpalomares@gmail.com>
2022-07-24 22:18:34 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pr_elecciones.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
